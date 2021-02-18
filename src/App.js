f8c0a067c2a7df1198d78a6e557422b15fa30bec
Armaan <armaanmaani@gmail.com>
2021-02-17 22:45:15 -0500

    Search Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import SearchPage from "./components/SearchPage";
 
 function App() {
-  //BEM
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search" component={SearchPage} />
+          <Route path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
